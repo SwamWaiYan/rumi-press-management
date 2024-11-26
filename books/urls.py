6b23d470d7ada0e05529d931d21f27dc0bf1b11e
Swam <swamwaiyan43@gmail.com>
2024-11-26 20:13:03 +0700

    Complete Django book distribution project with total expense calculation by category

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import report_view
 
 urlpatterns = [
-    path('report/', report_view, name='book-report'),
+    path('', report_view, name='book-report'),
 ]
